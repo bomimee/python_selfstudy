9bbc1a6273c20f7c1b208fb7d38c78c2eabca374
bomv2020 <bomi.kim@sobetec.com>
2025-12-16 16:44:55 +0900

    third commit

--- day51-twitter/main.py
+++ day51-twitter/main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-
+from time import sleep
 
 PROMISED_DOWN = 150
 PROMISED_UP = 10
@@ -13,5 +13,33 @@ TWITTER_PASSWORD = "YOI18@nate"
 class InternetSpeedTwitterBot:
     def __init__(self):
         self.service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service)
+        self.driver = webdriver.Chrome(service=self.service)
+        self.down = 0
+        self.up = 0
+    
+    def get_internet_speed(self):
+        self.driver.get('https://www.speedtest.net/result/18603130414')
+        go_button = self.driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[2]/a/span[4]')
+        go_button.click()
+        sleep(100)
+        datas = self.driver.find_elements(By.CLASS_NAME, 'result-data-value')
+        print(datas[0].text)
+        print(datas[1].text)
+        
+    def tweet_at_provider(self):
+        self.driver.get('https://x.com/home')
+        tweet_compose = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        sleep(3)
 
+        tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+
+        sleep(2)
+        self.driver.quit()
+
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
